aa88523884e5d8a98fbec5db913aec884cd3f07a
teppah <yifengyang01@gmail.com>
2019-01-29 22:05:44 -0500

    main application

--- src/main/java/com/micreau/test/api/apiv0/ApiV0Application.java
+++ src/main/java/com/micreau/test/api/apiv0/ApiV0Application.java
@@ -2,8 +2,10 @@ package com.micreau.test.api.apiv0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiV0Application {
 
     public static void main(String[] args) {
