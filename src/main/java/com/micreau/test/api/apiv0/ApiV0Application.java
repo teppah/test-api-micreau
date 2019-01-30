a404fc354f573df362fc42008cf6e2c458d8061e
teppah <yifengyang01@gmail.com>
2019-01-29 21:23:48 -0500

    initial intellij commit

---
+++ src/main/java/com/micreau/test/api/apiv0/ApiV0Application.java
@@ -0,0 +1,14 @@
+package com.micreau.test.api.apiv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiV0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiV0Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/micreau/test/api/apiv0/ApiV0ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.micreau.test.api.apiv0;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiV0ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
